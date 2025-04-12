d733dea015f9f4ff3e01d030417b290a7e5562f3
Ng Wei Jian <151597999+ngweijian0610@users.noreply.github.com>
2025-04-12 13:25:23 +0800

    Add files via upload

---
+++ Assignment/src/assignment/Customer.java
@@ -0,0 +1,22 @@
+package assignment;
+
+public class Customer extends User {
+    // data properties
+    private String customerID;
+//  private Cart cart;
+    
+    // methods
+    public Customer(){
+        
+    }
+    
+    // getter
+    public String getCustomerID(){
+        return customerID;
+    }
+    
+    // setter
+    public void setCustomerID(){
+        this.customerID = customerID;
+    }
+}
---
+++ Assignment/src/assignment/Order.java
@@ -0,0 +1,18 @@
+package assignment;
+
+public class Order {
+    // data properties
+    
+    
+    // methods
+    
+    
+    // getter
+    
+    
+    // setter
+    
+    
+    // other methods
+    
+}
