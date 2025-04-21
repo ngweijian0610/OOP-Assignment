cc2cd7f82f7d4c4f6575caefbfea63fc0ec58979
ngwei <ngaklol2@gmail.com>
2025-04-21 11:13:27 +0800

    fail ler

--- src/assignment/Assignment.java
+++ src/assignment/Assignment.java
@@ -1,13 +1,8 @@
 package assignment;
 
 public class Assignment {
-// cb
     public static void main(String[] args) {
-        //gg
+        
     }
-    //321
-    // chicken ass
-    // akosndojansd
-    // nelson here liao
 }
 
